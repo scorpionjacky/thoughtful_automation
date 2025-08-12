08fc217a38dd958a002334ce5547987f3ec57fb8
Mike Chen <chenmq2@yahoo.com>
2025-08-12 06:08:57 -0700

    Update platform_inteview.py

--- platform_inteview.py
+++ platform_inteview.py
@@ -1,14 +1,17 @@
-def sort(width = 0, height = 0, length = 0, mass = 0):
+def sort(width:int = 0, height:int = 0, length:int = 0, mass:int = 0):
     """
     w*h*l >= 1M or max([w,h,l]) >= 150: is_bulky
     mass >= 20: is_heavy
     is_bulky/is_heavy: t/t | t/f | f/t | f/f
-    ff: STANDARD; tt: REJECT; t/f f/t: SPECIAL
+    ff: STANDARD; tt: REJECTED; t/f f/t: SPECIAL
     """
     break_running = False
     delivery = None
     if None in [width, height, length, mass]:
         print("no input can be None")
+        break_running = True
+    if not break_running and not all(isinstance(v, int) for v in [width, height, length, mass]):
+        print("all input must be integer type")
         break_running = True
     if not break_running and min([width, height, length, mass]) <= 0:
         print("all input must be larger than 0")
